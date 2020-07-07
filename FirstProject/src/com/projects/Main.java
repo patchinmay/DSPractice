b7ebcc3dc50cb37b5c40ba8eda5902d6b133d34a
Chinmay <patchinmay152@gmail.com>
2020-07-07 00:22:43 -0400

    pushing first project in this repository

---
+++ FirstProject/src/com/projects/Main.java
@@ -0,0 +1,9 @@
+package com.projects;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("lets keep this repository for all the datastructure problems");
+    }
+}
